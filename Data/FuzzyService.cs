93ee66fbf0d239522bf4b5c0fe7e79042952becc
barteksmolec12 <barteksmolec@o2.pl>
2022-11-15 23:46:08 +0100

    Inital project

---
+++ Data/FuzzyService.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using System;
+using FuzzySharp;
+
+namespace BlazorWebAssembly.Data
+{
+    public class FuzzyService
+    {
+		public Task<int> GetScore(string value1, string value2)
+		{
+			return Task.FromResult(Fuzz.Ratio(value1, value2));
+		}
+	}
+}
